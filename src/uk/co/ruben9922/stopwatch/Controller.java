d05eebc63f05165127ce8445e5f038800758f77d
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-06-02 13:56:15 +0100

    Initial commit with simple UI

---
+++ src/uk/co/ruben9922/stopwatch/Controller.java
@@ -0,0 +1,13 @@
+package uk.co.ruben9922.stopwatch;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Spinner;
+
+public class Controller {
+    @FXML
+    private Spinner<Integer> hoursSpinner;
+    @FXML
+    private Spinner<Integer> minutesSpinner;
+    @FXML
+    private Spinner<Integer> secondsSpinner;
+}
