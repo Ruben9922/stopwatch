7814894f766d326c2d4327731e2fa958b2c1fac8
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-06-02 14:16:08 +0100

    Toggle displaying of elements & add Stop button

--- src/uk/co/ruben9922/stopwatch/Controller.java
+++ src/uk/co/ruben9922/stopwatch/Controller.java
@@ -1,6 +1,8 @@
 package uk.co.ruben9922.stopwatch;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 
 public class Controller {
@@ -10,4 +12,58 @@ public class Controller {
     private Spinner<Integer> minutesSpinner;
     @FXML
     private Spinner<Integer> secondsSpinner;
+    @FXML
+    private Label hoursLeftLabel;
+    @FXML
+    private Label minutesLeftLabel;
+    @FXML
+    private Label secondsLeftLabel;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button stopButton;
+
+    public void startButtonAction() {
+        startButton.setVisible(false);
+        startButton.setManaged(false);
+
+        stopButton.setVisible(true);
+        stopButton.setManaged(true);
+
+        hoursSpinner.setVisible(false);
+        minutesSpinner.setVisible(false);
+        secondsSpinner.setVisible(false);
+        hoursSpinner.setManaged(false);
+        minutesSpinner.setManaged(false);
+        secondsSpinner.setManaged(false);
+
+        hoursLeftLabel.setVisible(true);
+        minutesLeftLabel.setVisible(true);
+        secondsLeftLabel.setVisible(true);
+        hoursLeftLabel.setManaged(true);
+        minutesLeftLabel.setManaged(true);
+        secondsLeftLabel.setManaged(true);
+    }
+
+    public void stopButtonAction() {
+        stopButton.setVisible(false);
+        stopButton.setVisible(false);
+
+        startButton.setVisible(true);
+        startButton.setVisible(true);
+
+        hoursSpinner.setVisible(true);
+        minutesSpinner.setVisible(true);
+        secondsSpinner.setVisible(true);
+        hoursSpinner.setManaged(true);
+        minutesSpinner.setManaged(true);
+        secondsSpinner.setManaged(true);
+
+        hoursLeftLabel.setVisible(false);
+        minutesLeftLabel.setVisible(false);
+        secondsLeftLabel.setVisible(false);
+        hoursLeftLabel.setManaged(false);
+        minutesLeftLabel.setManaged(false);
+        secondsLeftLabel.setManaged(false);
+    }
 }
